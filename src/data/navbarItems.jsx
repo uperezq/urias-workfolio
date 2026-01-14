1c8238866d9ad1cf0bf8f239449d44cdb35d2900
U-Perez <uriasq.perez@gmail.com>
2026-01-14 13:50:23 -0500

    feat(navbar): add navbar section

---
+++ src/data/navbarItems.jsx
@@ -0,0 +1,28 @@
+export const items = [
+    {
+        label: "About",
+        bgColor: "#0D0716",
+        textColor: "#fff",
+        links: [
+            { label: "Profile", ariaLabel: "About Profile" },
+            { label: "Experience", ariaLabel: "About Experience" }
+        ]
+    },
+    {
+        label: "Projects",
+        bgColor: "#170D27",
+        textColor: "#fff",
+        links: [
+            { label: "Featured", ariaLabel: "Featured Projects" },
+            { label: "Github", ariaLabel: "Github Projects" }
+        ]
+    },
+    {
+        label: "skills",
+        bgColor: "#271E37",
+        textColor: "#fff",
+        links: [
+            { label: "Tools", ariaLabel: "About Tools" },
+        ]
+    }
+];
